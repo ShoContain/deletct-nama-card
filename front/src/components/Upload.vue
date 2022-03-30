a2e0e32a51c3b58912897981d27756ef2021895f
ShoContain <perblue.666@gmail.com>
2022-03-30 11:59:43 +0900

    add: アップロード画像表示

--- front/src/components/Upload.vue
+++ front/src/components/Upload.vue
@@ -3,23 +3,22 @@ import { ref } from "vue"
 import { ElMessageBox } from "element-plus"
 import type { UploadProps, UploadUserFile } from "element-plus"
 
-const fileList = ref<UploadUserFile[]>([
-    {
-        name: "food.jpeg",
-        url: "https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100",
-    },
-    {
-        name: "food2.jpeg",
-        url: "https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100",
-    },
-])
+const fileList = ref<UploadUserFile[]>([])
+const dialogVisible = ref(false)
+const dialogImageUrl = ref("")
 
 const handleRemove: UploadProps["onRemove"] = (file, uploadFiles) => {
     console.log(file, uploadFiles)
 }
 
 const handlePreview: UploadProps["onPreview"] = (uploadFile) => {
-    console.log(uploadFile)
+    dialogImageUrl.value =
+        "http://localhost:5010/static/task/" +
+        uploadFile.response.result.image.task_id +
+        "/" +
+        uploadFile.response.result.image.id + 
+        ".jpg"
+    dialogVisible.value = true
 }
 
 const beforeRemove: UploadProps["beforeRemove"] = (uploadFile, uploadFiles) => {
@@ -35,12 +34,12 @@ const beforeRemove: UploadProps["beforeRemove"] = (uploadFile, uploadFiles) => {
 <template>
     <el-upload
         action="http://localhost:5010/upload_image"
+        :file-list="fileList"
         name="uploadFile"
         :on-preview="handlePreview"
         :on-remove="handleRemove"
         :before-remove="beforeRemove"
         multiple
-        :file-list="fileList"
     >
         <el-button type="primary">アップロード</el-button>
         <template #tip>
@@ -49,4 +48,7 @@ const beforeRemove: UploadProps["beforeRemove"] = (uploadFile, uploadFiles) => {
             </div>
         </template>
     </el-upload>
+    <el-dialog v-model="dialogVisible">
+        <img w-full :src="dialogImageUrl" alt="Preview Image" />
+    </el-dialog>
 </template>
